47f8f57c25884e1e2050693a00d92e462110f2ca
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2020-02-16 19:05:11 -0500

    connected socket to server

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const path = require('path')
 const express = require('express')
+var http = require('http');
+const sockets = require('./sockets')
 
-const app = express ()
-const staticPath = path.join(__dirname,'..', 'public')
-app.use(express.static(staticPath))
-PORT = process.env.PORT || 3000
+const app = express ();
+const server = http.createServer(app);
+sockets(server);
 
-app.listen(PORT, () => {
+const staticPath = path.join(__dirname,'..', 'public');
+app.use(express.static(staticPath));
+
+PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
     console.log(`Listening on PORT: ${PORT}`);
-})
+});
---
+++ src/sockets.js
@@ -0,0 +1,10 @@
+const SocketIO = require('socket.io');
+
+
+module.exports = (server) => {
+    const io = SocketIO(server);
+
+    io.on('connection', (socket) => {
+        console.log('a user connected');
+      });
+}
