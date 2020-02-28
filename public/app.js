b2b096e53357bb2218f44be6e24cd4092349d739
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2020-02-28 02:46:10 -0500

    ducks appear random locations on refresh and dissappear when clicked on

--- public/app.js
+++ public/app.js
@@ -1,1 +1,34 @@
+const zoo = document.querySelector('.zoo');
 const socket = io();
+
+const ducksById = {};
+
+const collectDuck = (duck) => {
+    return () => {
+        socket.emit('collect-duck', {
+            id: duck.id,
+        });
+        if (ducksById[duck.id]) {
+            ducksById[duck.id].remove()
+        }
+    }
+}
+
+const updateView = (gameState) => {
+    gameState.ducks.forEach((duck) => {
+        if(!ducksById[duck.id]) {
+            const duckElement = document.createElement('span')
+            duckElement.classList.add('emoji')
+            duckElement.classList.add('animal')
+            duckElement.textContent = duck .emoji;
+            ducksById[duck.id] = duckElement
+            duckElement.addEventListener('click', collectDuck(duck))
+            zoo.appendChild(duckElement)
+        }
+        ducksById[duck.id].style.top = duck.location.y * window.innerHeight + 'px'
+        ducksById[duck.id].style.left = duck.location.x * window.innerWidth + 'px'
+
+    })
+    // console.log(gameState)
+}
+socket.on('game-state', updateView)
--- src/sockets.js
+++ src/sockets.js
@@ -4,7 +4,55 @@ const SocketIO = require('socket.io');
 module.exports = (server) => {
     const io = SocketIO(server);
 
+    const gameState = {
+        ducks: [{
+            id: 0,
+            emoji: "🦆",
+            location: {
+                x: Math.random(),
+                y: Math.random(),
+            }
+        },
+        {
+            id: 1,
+            emoji: "🦆",
+            location: {
+                x: Math.random(),
+                y: Math.random(),
+            }
+        },
+        {
+            id: 2,
+            emoji: "🦆",
+            location: {
+                x: Math.random(),
+                y: Math.random(),
+            }
+        },
+        {
+            id: 3,
+            emoji: "🦆",
+            location: {
+                x: Math.random(),
+                y: Math.random(),
+            }
+        },
+        {
+            id: 4,
+            emoji: "🦆",
+            location: {
+                x: Math.random(),
+                y: Math.random(),
+            }
+        }
+    ]
+    };
+
     io.on('connection', (socket) => {
         console.log('a user connected');
-      });
+    });
+
+    setInterval(() => {
+        io.emit('game-state', gameState)
+    }, 1000)
 }
